8660fb1c815464b5d1649b68bf60ce807dcba02b
sirKaruga <tonnydennis443@gmail.com>
2020-06-14 12:16:52 +0300

    Add files via upload

---
+++ build/precache-manifest.15f0d9785b2b043cba678ed0484bbe25.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc722069e0bcb6b4f7c9e773b1fbb08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd4459123db6ec04370",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "654d94cb3e9fb444d088",
+    "url": "/static/css/main.faa6a2f5.chunk.css"
+  },
+  {
+    "revision": "ddd4459123db6ec04370",
+    "url": "/static/js/2.aae2b548.chunk.js"
+  },
+  {
+    "revision": "86f96943d6537349861c4002a14026e3",
+    "url": "/static/js/2.aae2b548.chunk.js.LICENSE"
+  },
+  {
+    "revision": "654d94cb3e9fb444d088",
+    "url": "/static/js/main.c129ba7b.chunk.js"
+  },
+  {
+    "revision": "87e3e9af65dbdd559e79",
+    "url": "/static/js/runtime-main.9078eed4.js"
+  },
+  {
+    "revision": "5e97d5245e76d13cba972c9f1bdc8d26",
+    "url": "/static/media/Three-members.5e97d524.jpg"
+  },
+  {
+    "revision": "7ff561ee44684f03339d6745ccc33088",
+    "url": "/static/media/cardfume.7ff561ee.jpg"
+  },
+  {
+    "revision": "85f203f8809624a3690ff68d3d5d9f01",
+    "url": "/static/media/commercial-cleaning.85f203f8.jpg"
+  },
+  {
+    "revision": "b22201a15a48f627c29e0d534726e839",
+    "url": "/static/media/copyright.b22201a1.jpg"
+  },
+  {
+    "revision": "54577d4f0487a1910dee411cc6a0242b",
+    "url": "/static/media/dscaping.54577d4f.jpg"
+  },
+  {
+    "revision": "7b37a58c6d9403ff91fe4edd0861e886",
+    "url": "/static/media/email.7b37a58c.jpg"
+  },
+  {
+    "revision": "668acdebaf987f8e123adbb58d4b9cc7",
+    "url": "/static/media/fume3.668acdeb.jpg"
+  },
+  {
+    "revision": "95487f0353bac0d6bbda83f0707c18bd",
+    "url": "/static/media/mop2.95487f03.jpg"
+  },
+  {
+    "revision": "604435b0d3203f4367bbd6ce42fa35c2",
+    "url": "/static/media/s4.604435b0.jpeg"
+  },
+  {
+    "revision": "e2a6fad742ab7243c3c1c931e9a86a60",
+    "url": "/static/media/scapesm.e2a6fad7.jpg"
+  },
+  {
+    "revision": "c3851b850d358774d76d4503222bcb8f",
+    "url": "/static/media/scaping.c3851b85.png"
+  }
+]);
